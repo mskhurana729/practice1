1a57cd7d6c506f4dc6204355e4c10fe3fa7a2f66
Manpreet Singh <mskhurana729@gmail.com>
2025-09-19 18:03:51 -0400

    feat: Practice automating responsive images using cloudinary

---
+++ css/responsiveDesign/index.js
@@ -0,0 +1,10 @@
+(() => {
+  const baseUrl = 'https://res.cloudinary.com/fec-demo/image/fetch';
+  Array.from(document.querySelectorAll('[data-bg]')).forEach((image) => {
+    const { clientWidth, clientHeight } = image;
+    const pixelRatio = window.devicePixelRatio || 1.0;
+    const imageParams = `w_${100 * Math.round((clientWidth * pixelRatio) / 100)},h_${100 * Math.round((clientHeight * pixelRatio) / 100)},c_fill,g_auto,f_auto`;
+    const url = `${baseUrl}/${imageParams}/${image.dataset.bg}`;
+    image.style.backgroundImage = `url('${url}')`;
+  });
+})();
